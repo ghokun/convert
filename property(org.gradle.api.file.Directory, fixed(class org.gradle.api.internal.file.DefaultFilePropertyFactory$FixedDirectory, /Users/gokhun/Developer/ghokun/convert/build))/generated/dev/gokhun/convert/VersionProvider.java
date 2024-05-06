0ce4fd986eb71e3442f578427f4afb4d03a29453
Gokhun Celik <gokhun@gmail.com>
2024-05-06 14:49:09 +0200

    Revert "Remove gradle warnings"
    This reverts commit 6e9c262b2e0ab9deebd393611c12b3ba40d74dbe.

--- property(org.gradle.api.file.Directory, fixed(class org.gradle.api.internal.file.DefaultFilePropertyFactory$FixedDirectory, /Users/gokhun/Developer/ghokun/convert/build))/generated/dev/gokhun/convert/VersionProvider.java
+++
@@ -1,9 +0,0 @@
-
-package dev.gokhun.convert;
-
-final class VersionProvider implements picocli.CommandLine.IVersionProvider {
-    @Override
-    public String[] getVersion() throws Exception {
-        return new String[] {"0.0.0-SNAPSHOT"};
-    }
-}
